c1071240f67e73e42fd4c369216f2c2bcdf35108
kathy2470 <mukisakathy@gmail.com>
2024-07-09 20:06:35 +0300

    1-async_comprehension.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -18,19 +18,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Yields:
         float: A random number between 0 and 10.
     """
-    random.seed(0)
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-if __name__ == "__main__":
-    async def print_yielded_values() -> None:
-        """
-        Collects and prints values yielded by async_generator coroutine.
-        """
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
